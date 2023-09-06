833d2ca97acfdf5f601738fb5a6a7b6cb9cb8677
Rajat nautiyal <snautiyal048@gmail.com>
2023-09-06 16:06:21 +0530

    2nd commit of App.js file

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {MainPage} from './NEWS_site/mainNewsPage'
+import { useEffect, useState } from "react";
+import Button from "./LEARN/posts/createpost";
+import Grid from "./LEARN/posts/P1";
+import axios from "axios";
+
+function App() {
+
+  return (
+    <div>
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;

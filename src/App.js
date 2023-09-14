4043f25731a468c2a1401f5fb26b85fefe0527d5
Rajat nautiyal <snautiyal048@gmail.com>
2023-09-14 09:31:34 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import {MainPage} from './NEWS_site/mainNewsPage'
-import { useEffect, useState } from "react";
-import Button from "./LEARN/posts/createpost";
-import Grid from "./LEARN/posts/P1";
 import axios from "axios";
 
 function App() {

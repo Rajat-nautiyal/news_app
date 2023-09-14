cce0dbbb8fa3b67ecd6b4a9c875d42de17ad2c70
Rajat nautiyal <snautiyal048@gmail.com>
2023-09-14 09:29:07 +0530

    'headlines'

--- src/NEWS_site/NewsHeadings.js
+++ src/NEWS_site/NewsHeadings.js
@@ -1,24 +1,38 @@
-
 import React, { useContext } from "react";
 import {dataContext} from "./createContextTwo";
  
 
 export const Headings = ({ handleMainPage }) => {
   const articleData = useContext(dataContext);
+
+  const scrollToTop = () => {
+    if(window.screen.width < 1025){
+      window.scrollTo({
+      top: 0,
+    })
+    } else return
+  }
   return (
-    <> 
-      
+    <>  
       <div className="AllinOneHeadings">
-        {articleData.map((article) => (
-          <h4
-            className="eachHeading" onClick={() => handleMainPage(article)} key={article.url}
-          >                              {/* handleMainPage is used to send data of title to main page */}
-            <img
-                src={article.image}
-                height="80px"
-                width='240px'
-              /><div>{article.title}</div>
-          </h4>
+        {articleData.map((article, index) => (
+            <h4
+                className="eachHeading"  onClick={() => {scrollToTop(); handleMainPage(article);}} key={index}
+              >            {/* handleMainPage function is used to send data to main page */}
+                <img
+                    src={article.image}
+                    className="imageHeadings"
+                    height="80px"
+                    width='240px'
+
+                  />
+                  <div>
+                      <div className="articleTitle">{article.title}</div>
+                      <div className="publishDate">
+                        <div>Published on</div><div>{article.publishedAt}</div>
+                    </div>
+                  </div>
+              </h4>
         ))}
       </div>
     </>

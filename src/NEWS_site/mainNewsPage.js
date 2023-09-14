76e95a0a5c7a1aa3d1792f0483a3e7f3acb478dc
Rajat nautiyal <snautiyal048@gmail.com>
2023-09-14 09:31:10 +0530

    main page

--- src/NEWS_site/mainNewsPage.js
+++ src/NEWS_site/mainNewsPage.js
@@ -1,6 +1,5 @@
-import React, { useContext, useState,useEffect } from "react";
+import React, { useState,useEffect } from "react";
 import './News.css';
-
 import { Headings } from './NewsHeadings';
 import { dataContext } from "./createContextTwo";
 
@@ -24,37 +23,62 @@ export const MainPage = () => {
 
    const date = `${Y}-${M}-${D}`
    console.log(date)
-      
-    if (type === "Business") {
-      API = `https://gnews.io/api/v4/search?q=Business&lang=en&min=10&apikey=9d95187d657b5c056be45553cbece5ce`;
-    } else if (type === "Celebrities") {
-      API = `https://gnews.io/api/v4/search?q=Celebrities&lang=en&min=10&apikey=9d95187d657b5c056be45553cbece5ce`;
-    } else if (type === `Technology`) {
-      API = `https://gnews.io/api/v4/search?q=technology&lang=en&min=10&apikey=9d95187d657b5c056be45553cbece5ce`;
-    } else if (type === `Education`) {
-      API = `https://gnews.io/api/v4/search?q=Education&lang=en&min=10&apikey=9d95187d657b5c056be45553cbece5ce`;
-    }else if (type === `india`) {
-      API = `https://gnews.io/api/v4/top-headlines?country=in&lang=en&apikey=9d95187d657b5c056be45553cbece5ce`;
-    }else if (type === `japan`) {
-      API = `https://gnews.io/api/v4/top-headlines?country=jp&apikey=9d95187d657b5c056be45553cbece5ce`;
-    }else if (type === `USA`) {
-      API = `https://gnews.io/api/v4/top-headlines?country=us&lang=en&apikey=9d95187d657b5c056be45553cbece5ce`;
-    }else if (type === `russia`) {
-      API = `https://gnews.io/api/v4/top-headlines?country=ru&lang=en&apikey=9d95187d657b5c056be45553cbece5ce`;
-    }else if (type === `spain`) {
-      API = `https://gnews.io/api/v4/top-headlines?country=kr&apikey=9d95187d657b5c056be45553cbece5ce`;
-    }else if (type === `canada`) {
-      API = `https://gnews.io/api/v4/top-headlines?country=ca&lang=en&apikey=9d95187d657b5c056be45553cbece5ce`;
-    }else if (type === `britain`) {
-      API = `https://gnews.io/api/v4/top-headlines?country=gb&lang=en&apikey=9d95187d657b5c056be45553cbece5ce`;
+
+    switch (type){
+        case "Business":
+        API = `https://gnews.io/api/v4/search?q=Business&lang=en&min=10&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "Technology":
+        API = `https://gnews.io/api/v4/search?q=Technology&lang=en&min=10&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "fashion":
+        API = `https://gnews.io/api/v4/search?q=fashion&lang=en&min=10&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "Education":
+        API = `https://gnews.io/api/v4/search?q=Education&lang=en&min=10&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "Movies":
+        API = `https://gnews.io/api/v4/search?q=Movies&lang=en&min=10&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "Lifestyle":
+        API = `https://gnews.io/api/v4/search?q=Lifestyle&lang=en&min=10&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "health":
+        API = `https://gnews.io/api/v4/search?q=health&lang=en&min=10&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "entertainment":
+        API = `https://gnews.io/api/v4/search?q=entertainment&lang=en&min=10&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "USA":
+        API = `https://gnews.io/api/v4/top-headlines?country=us&lang=en&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "india":
+        API = `https://gnews.io/api/v4/top-headlines?country=in&lang=en&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "japan":
+        API = `https://gnews.io/api/v4/top-headlines?country=jp&lang=en&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "singapore":
+        API = `https://gnews.io/api/v4/top-headlines?country=sg&lang=en&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "Canada":
+        API = `https://gnews.io/api/v4/top-headlines?country=ca&lang=en&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "britain":
+        API = `https://gnews.io/api/v4/top-headlines?country=gb&lang=en&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        case "china":
+        API = `https://gnews.io/api/v4/top-headlines?country=cn&apikey=${process.env.REACT_APP_API_KEY}`;
+        break;
+        default :
+        API = `https://gnews.io/api/v4/search?q=world&lang=en&min=10&apikey=${process.env.REACT_APP_API_KEY}`
+        setSelectedArticle(null);
+
     }
-    else{ API = `https://api.worldnewsapi.com/search-news?number=20&api-key=a3fd4e534caf46b9a25f375049c128de&text=us`
-          setSelectedArticle(null) };
-          // https://gnews.io/api/v4/search?q=world&lang=en&min=10&apikey=9d95187d657b5c056be45553cbece5ce
     try {
       const response = await fetch(API);
       const data = await response.json();
-      setData(data.news); //passing fetched data to articleData(which is used in useState)
+      setData(data.articles);
       setSelectedArticle(null)
     } catch (error) {
       console.log(error);
@@ -89,28 +113,33 @@ useEffect(() => {
       <div className="header">
         <h1 className="quickGlance">QuickGlance</h1>
       </div>
-      <div className="parentElement">
         <div className="NavNheader">
           <nav className="Navbar">
-          <div className="dropdown" style={{ position: 'relative' }}>
+          <div className="dropdown" >
               <div className="country"><div id ='content'></div><div className="country-child">Country</div></div>
               <div className="dropdown-content">     
                   <div  onClick={()=>specificTopic("india")}>India</div>
                   <div  onClick={()=>specificTopic("USA")}>USA</div>
-                  <div  onClick={()=>specificTopic("spain")}>Spain</div> 
+                  <div  onClick={()=>specificTopic("singapore")}>Singapore</div> 
                   <div  onClick={()=>specificTopic("japan")}>Japan</div>
                   <div  onClick={()=>specificTopic("britain")}>Britain</div>
-                  <div  onClick={()=>specificTopic("canada")}>Canada</div> 
+                  <div  onClick={()=>specificTopic("Canada")}>Canada</div> 
+                  <div  onClick={()=>specificTopic("china")}>china</div> 
+
               </div>
            </div>         
             <li  onClick={() =>specificTopic("Technology")}>Technology</li>
             <li  onClick={() =>specificTopic("Business") }>Business</li>
-            <li  onClick={()=>specificTopic("Celebrities")}>Personalities</li>
+            <li  onClick={()=>specificTopic("fashion")}>fashion</li>
             <li  onClick={()=>specificTopic("Education")}>Education</li>
+            <li  onClick={()=>specificTopic("Movies")}>Movies</li>
+            <li  onClick={()=>specificTopic("health")}>Health</li>
+            <li  onClick={()=>specificTopic("entertainment")}>Entertainment</li>
+            <li  onClick={()=>specificTopic("Lifestyle")}>Lifestyle</li>
+
           </nav>
-          <div className="parent-top-headline"><h3 className="Top-Headline">Top Headlines</h3></div>
+            <div className="parent-TH"><h3 className="Top-Headline">Top Headlines</h3></div>
         </div>
-      </div>
 
       <div className="paraNheadings">
         {selectedArticle ? (
@@ -120,8 +149,7 @@ useEffect(() => {
               <img
                 src={selectedArticle.image}
                 alt="Loading"
-                height="340px"
-                width="800px"
+                className="image"
               />
             </div>
             <p>{selectedArticle.description}</p>
@@ -131,6 +159,7 @@ useEffect(() => {
             <div>
               <div className="click" onClick={click}>click to read full article</div>
             </div>
+            <h3 className="mob_Top_headlines">Top Headlines</h3>
           </div>
         ) :articleData? (
           <div className="NewsPara">
@@ -139,17 +168,18 @@ useEffect(() => {
               <img
                 src={articleData[0].image}
                 alt="Loading"
-                height="340px"
-                width="800px"
+                className="image"
               />
             </div>
             <p style={{paddingBottom:"0px"}}>{articleData[0].description}</p>
-            <p style={{paddingBottom:"0px"}}>{articleData[0].content}</p>
+            <p >{articleData[0].content}</p>
 
             <div>
               <div className="click" onClick={click}>click to read full article</div>
             </div>
+            <h3 className="mob_Top_headlines">Top Headlines</h3>
           </div>
+            
         ):(<div className="NewsPara">Loading</div>) 
         }
       <dataContext.Provider value={articleData}>

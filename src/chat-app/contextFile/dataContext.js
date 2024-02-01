1e576dba5b94735168e09225b325647328a35617
Rajat N <133015146+Rajat-nautiyal@users.noreply.github.com>
2024-02-01 10:28:57 +0530

    Delete src/chat-app directory
    ignore it

--- src/chat-app/contextFile/dataContext.js
+++
@@ -1,7 +0,0 @@
-import {createContext} from "react";
-import {useState, useEffect} from 'react'
-import { addDoc, getDoc, setDoc,getDocs, collection, doc,query , where, serverTimestamp} from 'firebase/firestore';
-import {auth, firestore} from '../firebase'
-
-export const AllUserContext = createContext();
-
